72be5202b5508f0977d4eed3b1872b3435b7274d
PrinceBLVCK <princekarabo70@gmail.com>
2023-01-22 15:42:36 +0200

    Added Favorites

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import {View, Text, StyleSheet, ImageBackground} from 'react-native'
+import {View, Text, StyleSheet, ImageBackground, Button, Image} from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { LinearGradient } from 'expo-linear-gradient';
+import { HermesBadge } from "react-native/Libraries/NewAppScreen";
+import Entypo from 'react-native-vector-icons/Entypo'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+
 
 
 export default SearchScreen = () => {
@@ -22,7 +28,57 @@ export default SearchScreen = () => {
                     end={{x:0,y:.4}}
                     colors={[ '#343434', '#000000']}>
 
-                    <Ionicons name='md-ellipsis-horizontal-sharp' size={30} color={'#fffa'}/>
+                    <View style={{
+                        flex:.2,  
+                        paddingHorizontal:30, 
+                        paddingVertical:0, 
+                        flexDirection:'row', 
+                        justifyContent:'space-between',
+                        alignItems:'center',
+                        position:'relative',
+                        top:0,
+                    
+                        }}>
+                        <View style={{flex:.8,}}>
+                            <Text style={{flex:.2, position:'relative', color:'#fff6',marginBottom:10, fontSize:"14em"}}>Abalaleli benyanga anbangu-166,307</Text>
+                            <View style={{flex:.5, position:'relative', width:'100%',margin:0, flexDirection:'row'}}>
+                                <Text style={[styles.follow]}>OBALANDELAYO</Text>
+                                <Ionicons style={{marginLeft:20, padding:10, alignContent:'center', justifyContent:'center'}} name='md-ellipsis-horizontal-sharp' size={30} color={'#fffa'}/>
+                            </View>
+                        </View>
+                        <View style={[styles.play]}>
+                            <Ionicons style={{flex:.9, alignContent:'center', justifyContent:'center', }} name="play" size={30} color={'white'} />
+                            <View style={[styles.shuffle, ]}>
+                                <MaterialCommunityIcons style={{}} name="shuffle-variant" color={'#393'} size={20}/>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={[{flex:.2, flexDirection:"row", padding:20, paddingHorizontal:30}]}>
+                        <View>
+                            <Image 
+                                source={require('../assets/bg.jpg')}
+                                resizeMode="cover"
+                                style={{width:55, height:55, borderRadius:40, overflow:"hidden"}} />
+                            <View style={[styles.favorite]}>
+                                <MaterialIcons name="favorite" size={30} color="white" />
+                            </View>
+                        </View>
+                        <View style={{marginHorizontal:10 , flex:.8, flexDirection:"column", padding:10}}>
+                            <Text style={{color:"white", fontWeight:"bold", fontSize:"16em"}}>Izingoma Ezithandiwe</Text>
+                            <View style={{flex:1, flexDirection:"row", alignItems:"center", }}>
+                                <Text style={{color:'#fff6', fontSize:'14em'}}>5 izingoma </Text>
+                                <View style={[styles.dot, {width:5, height:5, marginHorizontal:2}]}/>
+                                <Text style={{color:'#fff6', fontSize:'14em'}}>High Klassified</Text>
+                            </View>
+                        </View>
+                        <View style={{flex:.2,flexDirection:'row',alignItems:'center', justifyContent:'flex-end'}}>
+                            <Entypo name="chevron-thin-right" size={30} color="#fff5"/>
+                        </View>
+                    </View>
+                    <View style={{flex:.4}}>
+                        
+                    </View>
+                    
                 </LinearGradient>
                 
 
@@ -57,7 +113,59 @@ const styles = StyleSheet.create({
         backgroundColor: "#00000080",
         opacity:1,
         borderRadius: 20
-    }
+    },
+    follow:{
+        color:'white', 
+        borderColor:'white', 
+        borderWidth:1,
+        padding: 10,
+        alignContent:'center',
+        justifyContent:'center',
+        borderRadius:10,
+        fontSize: "16em",
+        width:'auto',
+        fontWeight:'bold'
 
+    },
+    play:{
+        
+        width:55,
+        height:55,
+        padding:12,
+        backgroundColor:'#393',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:70,
+        flex:.1,
+    },
+    shuffle:{
+        position:'absolute',
+        top: '130%',
+        left: '110%',
+        padding:3,
+        flex:.3,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'white',
+        borderRadius:20
+    },
+    favorite:{
+        width:35,
+        height: 35,
+        borderRadius: 50,
+        backgroundColor:"#393",
+        alignItems:"center",
+        justifyContent:"center",
+        padding:3,
+        bottom:25,
+        left: 30
+
+    },
+    dot:{
+        borderRadius:100,
+        width: 50,
+        height:50,
+        backgroundColor: "#fff6"
+    }
 
 })
